29c57648f9c7efd752587b9c00df5b3e0e08e386
heydaytime <whynotmihir@gmail.com>
2025-06-24 19:40:47 +0530

    trying out colemak_dh_main_iso

--- keymap.c
+++ keymap.c
@@ -31,8 +31,11 @@ enum layers {
 
 enum custom_keycodes {
     CTL_B = SAFE_RANGE,
-    CTL_G
+    CTL_G,
+    MACRO_J,  // Custom J macro
+    MACRO_Z   // Custom Z macro
 };
+
 // https://getreuer.info/posts/keyboards/custom-shift-keys/index.html#add-custom-shift-keys-to-your-keymap
 const custom_shift_key_t custom_shift_keys[] = {
     {KC_EXLM, KC_1},    // ! -> 1 when Shift is held
@@ -57,17 +60,17 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [MAC_BASE] = LAYOUT_ansi_84(
      KC_ESC,   KC_BRID,  KC_BRIU,  KC_MCTRL, KC_LNPAD, RGB_VAD,  RGB_VAI,  KC_MPRV,  KC_MPLY,  KC_MNXT,  KC_MUTE,  KC_VOLD,  KC_VOLU,  KC_SNAP,  KC_DEL,   RGB_MOD,
      KC_GRV,KC_EXCLAIM,KC_AT,KC_HASH,KC_DOLLAR,KC_PERCENT,KC_CIRCUMFLEX,KC_AMPERSAND,KC_ASTERISK,KC_LEFT_PAREN,KC_RIGHT_PAREN,KC_MINS,KC_EQL,CTL_B,KC_PGUP,
-     KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_LBRC,  KC_RBRC,  KC_BSLS,            KC_PGDN,
-     KC_BACKSPACE,  KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     KC_COLON,  KC_QUOT,            KC_ENT,      KC_HOME,
-     KC_LSFT,            KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,           CTL_G,  KC_UP,    KC_END,
-     KC_LCTL,  KC_LOPTN, KC_LCMMD,                               KC_SPC,                                 KC_ESC,MO(MAC_FN),KC_ROPTN,  KC_LEFT,  KC_DOWN,  KC_RGHT),
+     KC_TAB,   KC_Q,     KC_W,     KC_F,     KC_P,     KC_B,     KC_J,     KC_L,     KC_U,     KC_Y,     KC_COLON,  KC_LBRC,  KC_RBRC,  KC_BSLS,            KC_PGDN,
+     KC_BACKSPACE,  KC_A,     KC_R,     KC_S,     KC_T,     KC_G,     KC_M,     KC_N,     KC_E,     KC_I,     KC_O,     KC_QUOT,            KC_ENT,      KC_HOME,
+     KC_LSFT,            KC_Z,     KC_X,     KC_C,     KC_D,     KC_V,     KC_K,     KC_H,     KC_COMM,  KC_DOT,   KC_SLSH,           CTL_G,  KC_UP,    KC_END,
+     KC_LCTL,  MO(MAC_FN), KC_LCMMD,                               KC_SPC,                                 KC_ESC,MO(MAC_FN),KC_ROPTN,  KC_LEFT,  KC_DOWN,  KC_RGHT),
 
 [MAC_FN] = LAYOUT_ansi_84(
      _______,  KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,   _______,  _______,  RGB_TOG,
      _______,  BT_HST1,  BT_HST2,  BT_HST3,  P2P4G,    _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,            _______,
-     RGB_TOG,  RGB_MOD,  RGB_VAI,  RGB_HUI,  RGB_SAI,  RGB_SPI,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,            _______,
-     _______,  RGB_RMOD, RGB_VAD,  RGB_HUD,  RGB_SAD,  RGB_SPD,  _______,  _______,  _______,  _______,  _______,  _______,            _______,            _______,
-     _______,            _______,  _______,  _______,  _______,  BAT_LVL,  NK_TOGG,  _______,  _______,  _______,  _______,            _______,  _______,  _______,
+     RGB_TOG,  KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     _______,  _______,  _______,            _______,
+     _______,  KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     MACRO_J,  KC_K,     KC_L,     KC_SCLN,  _______,            _______,            _______,
+     _______,            MACRO_Z,  KC_X,     KC_C,     KC_V,     KC_B,     KC_N,     KC_M,     _______,  _______,  _______,            _______,  _______,  _______,
      _______,  _______,  _______,                                _______,                                _______,  _______,  _______,  _______,  _______,  _______),
 
 [WIN_BASE] = LAYOUT_ansi_84(
@@ -88,8 +91,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 };
 
 // clang-format on
+
 // Track whether Left Shift is currently held
 static bool lshift_held = false;
+
+// Variables for macro key repeat functionality
+static bool macro_j_pressed = false;
+static bool macro_z_pressed = false;
+static uint16_t macro_j_timer = 0;
+static uint16_t macro_z_timer = 0;
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     if (!process_custom_shift_keys(keycode, record)) { return false; }
@@ -114,19 +124,61 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
                 SEND_STRING(SS_LCTL("b"));  // Sends Ctrl+B
             }
             return false;
-        break;
 
         case CTL_G:
             if (record->event.pressed) {
                 SEND_STRING(SS_LCTL("g"));  // Sends Ctrl+G
             }
             return false;
-        break;
 
+        case MACRO_J:
+            if (record->event.pressed) {
+                // Key pressed
+                macro_j_pressed = true;
+                macro_j_timer = timer_read();
+                register_code(KC_J);  // Register J key
+            } else {
+                // Key released
+                macro_j_pressed = false;
+                unregister_code(KC_J);  // Unregister J key
+            }
+            return false;
+
+        case MACRO_Z:
+            if (record->event.pressed) {
+                // Key pressed
+                macro_z_pressed = true;
+                macro_z_timer = timer_read();
+                register_code(KC_Z);  // Register Z key
+            } else {
+                // Key released
+                macro_z_pressed = false;
+                unregister_code(KC_Z);  // Unregister Z key
+            }
+            return false;
     }
 
     if (!process_record_keychron_common(keycode, record)) {
         return false;
     }
     return true;
+}
+
+// Handle key repeat functionality
+void matrix_scan_user(void) {
+    // Handle J key repeat
+    if (macro_j_pressed) {
+        if (timer_elapsed(macro_j_timer) > TAPPING_TERM) {
+            // Key has been held long enough, enable repeat
+            // The register_code above will handle the repeat automatically
+        }
+    }
+
+    // Handle Z key repeat
+    if (macro_z_pressed) {
+        if (timer_elapsed(macro_z_timer) > TAPPING_TERM) {
+            // Key has been held long enough, enable repeat
+            // The register_code above will handle the repeat automatically
+        }
+    }
 }
